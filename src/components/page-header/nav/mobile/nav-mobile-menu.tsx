24e6854c776d1d26eee391076ca1ee45aef2394c
Administrator <744890753@qq.com>
2025-12-20 14:07:14 +0800

    refactor(Dashboard): 重构页面结构
    - 将stake 改为ethers function 作为相关ethers 功能模块测试页面

--- src/components/page-header/nav/mobile/nav-mobile-menu.tsx
+++ src/components/page-header/nav/mobile/nav-mobile-menu.tsx
@@ -28,7 +28,7 @@ export default function NavMobileMenu({show, onClose}:{show: boolean, onClose: (
             Dashboard
           </span>
             </NavLink>
-            <NavLink href="/stake" onClick={() => onLinkClick("/stake")}>
+            <NavLink href="/ethers-function" onClick={() => onLinkClick("/ethers-function")}>
           <span className="flex items-center gap-2">
             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clipRule="evenodd" />
